72945851f7982d5c68138647f17c3559b4ccb422
MaryannWebb <mkwebb@hawaii.edu>
2024-11-03 20:57:34 -1000

    done setup for 3 figs, need to finalize them

--- Figure_1.py
+++ Figure_1.py
@@ -26,14 +26,13 @@ df1 = raw
 #calculate percent change in coral cover
 df1['coral_change_percent'] = (
     (df1['coral_cover_2100'] - df1['coral_cover_2020']) / df1['coral_cover_2020']
-) * 100
-
+)
 #Figure 1: A map showing the predicted percentage change in coral cover over the 21st century, averaged across simulations.       
 
 #getting mean percent cover for each geographic coordinate (https://stackoverflow.com/questions/75480113/python-using-pandas-to-take-average-of-same-lon-lat-value-pairs)
-lat_tup = (df1['latitude'])
-lon_tup = (df1['longitude'])
-cover_tup = (df1['coral_change_percent'])
+lat_tup = (raw['latitude'])
+lon_tup = (raw['longitude'])
+cover_tup = (raw['coral_change_percent'])
 lat_lon_cov = {
     'lat': lat_tup,
     'lon': lon_tup,
@@ -53,7 +52,8 @@ ax1 = plt.axes(projection=ccrs.PlateCarree())
 ax1.coastlines()
 ax1.scatter(df2_groups['lon'], df2_groups['lat'], c=df2_groups['cover'], transform=ccrs.PlateCarree(),
         alpha=0.1)
-#ax1.set_extent([-180, 180,-40,40])
+
+
 
 #need to do a few more things before ready 
         #make color scale more refined
@@ -63,3 +63,41 @@ ax1.scatter(df2_groups['lon'], df2_groups['lat'], c=df2_groups['cover'], transfo
 ###################################
 #      FIGURE 2: pH               #
 ####################################
+#A plot showing the predicted percentage change in coral cover over the 21st century (averaged across simulations) as a function of latitude.
+
+sns.set_theme(style="whitegrid")
+sns.scatterplot(data=df2_groups, x='lat', y='cover')
+
+
+###################################
+#       FIGURE 3:FACET            #
+###################################
+#first make a column for the overall change in coral cover 
+df3 = raw
+df3['coral_change'] = (df3['coral_cover_2020'])-(df3['coral_cover_2100'])
+#use groupby to aggregate by model
+df3_model = df3[['coral_change','pH_2020',"pH_2100",'model','coral_cover_2020','coral_cover_2100','SST_2020','SST_2100']].groupby(df3['model'])
+# Compute the mean
+df3_change_mean = df3_model.mean().drop(columns='model')
+df3_change_mean['model'] = ['0','1','2','3','4','5','6','7','8','9','10','11']
+
+#make a new dataframe in better format for plotting
+    #make a new dataframe to manipulate starting with the data from 2020
+df_2020 = df3_change_mean[['coral_cover_2020','pH_2020','SST_2020','model']]
+df_2020 = df_2020.rename(columns={"coral_cover_2020": "coral_cover","pH_2020":"pH","SST_2020":"SST"})
+
+df_2100 = df3_change_mean[['coral_cover_2100','pH_2100','SST_2100','model']] #same thing for 2100
+df_2100 = df_2100.rename(columns={"coral_cover_2100": "coral_cover","pH_2100":"pH","SST_2100":"SST"})
+#combine 2020 and 2100 datasets on top of each other so year become its own column rather than separate cols
+df3_join = pd.concat([df_2020, df_2100], axis=0)
+#add year column (there's got to be a better way *fix if I have time)
+df3_join['year'] = ['2020','2020','2020','2020','2020','2020','2020','2020','2020','2020','2020','2020',
+                '2100','2100','2100','2100','2100','2100','2100','2100','2100','2100','2100','2100']
+
+
+g = sns.FacetGrid(df3_join, col="year",hue="model",legend_out=True, palette="colorblind",height=3,aspect=.75)
+g.map(sns.scatterplot, "pH", "coral_cover", alpha=.7)
+g.set_titles(col_template="{col_name}")
+g.add_legend(title="Simulation")
+g.set_axis_labels(x_var="pH", y_var="Coral Cover (sq.km)", clear_inner=True)
+sns.move_legend(g,"upper right")
